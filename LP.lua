587634889ab4a12b8546f7bf574314955fe8ceb1
zis <jdjdjssh545@gmail.com>
2026-08-01 23:06:47 +0800

    Update LP.lua

--- LP.lua
+++ LP.lua
@@ -1,7 +1,7 @@
 -- เทส
 local Solar = nil
 local Success, Error = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/discounthee-sys/Zenith-Soul-hub/refs/heads/main/Solar.lua"))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Zentih-alt/Zentih-Soul-hub/refs/heads/main/Solar.lua"))()
 end)
 
 if not Success then
