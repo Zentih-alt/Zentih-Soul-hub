154d1bf49a3e5cd1b310d5046f5e7b96b6a4a303
zis <jdjdjssh545@gmail.com>
2026-08-02 22:38:37 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,18 @@
+-- [[ load.lua ]]
+local url = "https://raw.githubusercontent.com/Zentih-alt/Zentih-Soul-hub/main/Game.lua"
 
+local success, code = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success and code and #code > 0 then
+    local func, err = loadstring(code)
+    if func then
+        func()
+    else
+        warn("[Compile Error]:", err)
+        game:GetService("Players").LocalPlayer:Kick("load.lua: Compile Error")
+    end
+else
+    game:GetService("Players").LocalPlayer:Kick("load.lua: Failed to load Game.lua")
+end
