9baeafc4b6aa890c5118202efb29c4ca26a40077
zis <jdjdjssh545@gmail.com>
2026-08-03 11:09:11 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,18 +1,114 @@
--- [[ load.lua ]]
-local url = "https://raw.githubusercontent.com/Zentih-alt/Zentih-Soul-hub/main/Game.lua"
-
-local success, code = pcall(function()
-    return game:HttpGet(url)
-end)
-
-if success and code and #code > 0 then
-    local func, err = loadstring(code)
-    if func then
-        func()
-    else
-        warn("[Compile Error]:", err)
-        game:GetService("Players").LocalPlayer:Kick("load.lua: Compile Error")
-    end
+-- Protected by Mnx Obfuscator | Public Enemy
+local d9y0WBhPmH=tostring(21)
+local tZ8SUpZtXlhtqay00k=183
+local dW10CdVaH=type(nil)
+local bZwZ2wnfkQjda=math.abs(-2)
+local oosnuAkwQN1u=math.floor(8)
+local g7Errf76lnep=math.abs(-69)
+local kkEpv07k5trdhI1ME=string.len("rNFpCg")
+local uNrPTLaPsJ18M=math.abs(-43)
+local cmrgWBwkQn4qlMtvR8=math.floor(6)
+local aPB40ePO5nIMw=116
+local eaRgAc53F=type(nil)
+local fbHRgZvBk8a0=math.abs(-39)
+for _=1,0 do end
+if false then error("") end
+local pxQkwCrrdhLl=tostring(37)
+local juBman0t4edtxAKzK0=tostring(63)
+for _=1,0 do end
+local hmQC2d9DoVGt6J_={"4061c58645f760311af8974ac9f2c29291fae0012440a9ef384182f1ff676cf6d8e6a8f8c56393f852db039c78c8740df488a19090e262e","502e9b6ea4988f8c5b74e06d4107f239fd4fa97ec9db90ad5680a6dd","d000ffc20b940a9bbbc4057eb489a8c37cb8b7c1462c193cd9af9f8805a91d4c7570b8cf8f9105c17a20982eb5eb640756d28c8d0d6277f","b00243056d0b51fcc0efe85d69a65b18acc0679147b0d18b8a89e5ef6297a10f3bf0552de84d0d925653113ff34fa3c469562fb36047b94","0b27afea8127a9998007424219f11516055c939a5fc671df753b734749094c499d13ad6d5a2304bef01c11eb65e6c5c4a741f0321c7d067"}
+local b1yAlDaABaxLIrDhSD={2,5,1,3,4}
+local a1TGqQ_c84qv7WuYK={"a54a98d39bc277bc97777c3c1cb20e4086ade80b16074177c1c8d33f69d7cf8395c972fa4f84383d3ed2a1f93435b74f1f528d76c49c638","8eb34805bd784d8b035c5fab46e343ee9ab7ab9e80c96397d304757eba7ca78c00fe088f0e782b58d58dc1d926537ca1b652f784d11b8f7","c29accb6ddbc47e47b4c25d31e06fb23e0469a76d44a9509c28a5d","2609179a2e24932688aa220ecf38483cd1b6a8dfc8d4f83bbda779bd383444f42753f451e60f03308a68bd36a6e772248bd11dcd74c5c29","22e7d82d85a104e1ee336139fa295c93c472c58592170c8a734b3b1ef78397e1f91eb7b33d897aa0dcc2e24ef18f3d03e3f2c08ab28d5aa"}
+local lPK6twXaRXc0BkS={4,1,5,3,2}
+local ipaG8sRJKOA4qdfFn1=string.len("kUWNjbbK0X")
+local mARpw_I9XAp=bit32.bxor(115,95)
+local tF0zMes0BN=math.abs(-85)
+local fCJP5aEhNYP=bit32.bxor(85,196)
+for _=1,0 do end
+do local _=nil end
+local ccgw9eoZXnP5t={}
+for klUskuZEoUFIWr_=1,#b1yAlDaABaxLIrDhSD do ccgw9eoZXnP5t[b1yAlDaABaxLIrDhSD[klUskuZEoUFIWr_]]=hmQC2d9DoVGt6J_[klUskuZEoUFIWr_] end
+ccgw9eoZXnP5t=table.concat(ccgw9eoZXnP5t)
+local waXFb7ei4={}
+for klUskuZEoUFIWr_=1,#lPK6twXaRXc0BkS do waXFb7ei4[lPK6twXaRXc0BkS[klUskuZEoUFIWr_]]=a1TGqQ_c84qv7WuYK[klUskuZEoUFIWr_] end
+waXFb7ei4=table.concat(waXFb7ei4)
+local wZWX_cS9slWu={}
+for klUskuZEoUFIWr_=1,#ccgw9eoZXnP5t,2 do
+  wZWX_cS9slWu[#wZWX_cS9slWu+1]=tonumber(string.sub(ccgw9eoZXnP5t,klUskuZEoUFIWr_,klUskuZEoUFIWr_+1),16)
+end
+local uH3jYpV_GgJ2TCkwK={34,251,19,120,237,50,145,41,115,55,192,74,117,74,140,27,218,243,213,142,134,138,69,165,229,13,96,159,10,117,195,90,146,122,137,109,118,77,133}
+for klUskuZEoUFIWr_=1,#wZWX_cS9slWu do
+  wZWX_cS9slWu[klUskuZEoUFIWr_]=bit32.bxor(wZWX_cS9slWu[klUskuZEoUFIWr_],uH3jYpV_GgJ2TCkwK[(klUskuZEoUFIWr_-1)%#uH3jYpV_GgJ2TCkwK+1])
+end
+local anHtHz24NJVWDt={}
+for klUskuZEoUFIWr_=1,#waXFb7ei4,2 do
+  anHtHz24NJVWDt[#anHtHz24NJVWDt+1]=tonumber(string.sub(waXFb7ei4,klUskuZEoUFIWr_,klUskuZEoUFIWr_+1),16)
+end
+local wNvsE3DUJzo4oFffu={208,128,45,122,250,165,24,191,244,62,66,246,191,142,194,239,67,18,5,167,46,17,44,221,222,228,82,234,21,20,238,160,95,38,138,163,245,230,224}
+for klUskuZEoUFIWr_=1,#anHtHz24NJVWDt do
+  anHtHz24NJVWDt[klUskuZEoUFIWr_]=bit32.bxor(anHtHz24NJVWDt[klUskuZEoUFIWr_],wNvsE3DUJzo4oFffu[(klUskuZEoUFIWr_-1)%#wNvsE3DUJzo4oFffu+1])
+end
+local gt3znHoowN={}
+for klUskuZEoUFIWr_=1,math.max(#wZWX_cS9slWu,#anHtHz24NJVWDt) do
+  if wZWX_cS9slWu[klUskuZEoUFIWr_] then gt3znHoowN[#gt3znHoowN+1]=wZWX_cS9slWu[klUskuZEoUFIWr_] end
+  if anHtHz24NJVWDt[klUskuZEoUFIWr_] then gt3znHoowN[#gt3znHoowN+1]=anHtHz24NJVWDt[klUskuZEoUFIWr_] end
+end
+local r_r_V921KAopggK={}
+local aDe7YqIb3K8xBwfHv=854692538
+for klUskuZEoUFIWr_=#gt3znHoowN-1,1,-1 do
+  aDe7YqIb3K8xBwfHv=(aDe7YqIb3K8xBwfHv*1664525+1013904223)%4294967296
+  local lBKRPh2mZ=aDe7YqIb3K8xBwfHv%(klUskuZEoUFIWr_+1)+1
+  r_r_V921KAopggK[#r_r_V921KAopggK+1]={klUskuZEoUFIWr_+1,lBKRPh2mZ}
+end
+for klUskuZEoUFIWr_=#r_r_V921KAopggK,1,-1 do
+  local dxbaz4Ulfi7jGMEYLr=r_r_V921KAopggK[klUskuZEoUFIWr_]
+  local kXN1Y1Wvt0Ov=gt3znHoowN[dxbaz4Ulfi7jGMEYLr[1]]
+  gt3znHoowN[dxbaz4Ulfi7jGMEYLr[1]]=gt3znHoowN[dxbaz4Ulfi7jGMEYLr[2]]
+  gt3znHoowN[dxbaz4Ulfi7jGMEYLr[2]]=kXN1Y1Wvt0Ov
+end
+local aVoUSVCZlz_={}
+local vDewsujY0A=893669407
+for klUskuZEoUFIWr_=#gt3znHoowN-1,1,-1 do
+  vDewsujY0A=(vDewsujY0A*1664525+1013904223)%4294967296
+  local clVS9_6EcsAL=vDewsujY0A%(klUskuZEoUFIWr_+1)+1
+  aVoUSVCZlz_[#aVoUSVCZlz_+1]={klUskuZEoUFIWr_+1,clVS9_6EcsAL}
+end
+for klUskuZEoUFIWr_=#aVoUSVCZlz_,1,-1 do
+  local vJ1ySW8PODZ=aVoUSVCZlz_[klUskuZEoUFIWr_]
+  local rmr9GaoAdToZn=gt3znHoowN[vJ1ySW8PODZ[1]]
+  gt3znHoowN[vJ1ySW8PODZ[1]]=gt3znHoowN[vJ1ySW8PODZ[2]]
+  gt3znHoowN[vJ1ySW8PODZ[2]]=rmr9GaoAdToZn
+end
+local lLD5oW0EoZ0o={203,139,138,89,58,101,157,212,120,32,171,96,34,145,91,7,175,75,237,117,110,88,47,214,136,32,3,217,143,179,161,174,102,132,152,66,76,102,17}
+for klUskuZEoUFIWr_=1,#gt3znHoowN do
+  gt3znHoowN[klUskuZEoUFIWr_]=bit32.bxor(gt3znHoowN[klUskuZEoUFIWr_],lLD5oW0EoZ0o[(klUskuZEoUFIWr_-1)%#lLD5oW0EoZ0o+1])
+end
+local aJ747nma1P={8,138,62,213,53,112,163,169,177,56,42,73,238,209,32,198,106,164,12,170,249,14,14,122,79,36,185}
+for klUskuZEoUFIWr_=1,#gt3znHoowN do
+  gt3znHoowN[klUskuZEoUFIWr_]=bit32.bxor(gt3znHoowN[klUskuZEoUFIWr_],aJ747nma1P[(klUskuZEoUFIWr_-1)%#aJ747nma1P+1])
+end
+local w9iwS0DTjf=bit32.bxor(63,16)
+local vLSJLq_RUzt1wv0zj=#"fy2Gl"
+local lEhYvb0Ni=string.len("qdu_Hwy9TZ")
+local jlcYfPPhjn=tostring(20)
+local gNmJCmIacM4a=#"oDf0tb"
+local i3zRlCubwg3MY=math.floor(4)
+if false then error("") end
+for _=1,0 do end
+local gAobMGRWY8={107,53,136,212,197,174,54,6,90,183,100,179,230,132,114,76,63,134,106,45,123,88,136,231,141,106,188,141,9,250,232,96,160,29,150,111,123,6,111}
+local xFPt7sc67rOxUj9={}
+for klUskuZEoUFIWr_=1,#gt3znHoowN do
+  xFPt7sc67rOxUj9[klUskuZEoUFIWr_]=string.char(bit32.bxor(gt3znHoowN[klUskuZEoUFIWr_],gAobMGRWY8[(klUskuZEoUFIWr_-1)%#gAobMGRWY8+1]))
+end
+local hK_kAmuekip0y=table.concat(xFPt7sc67rOxUj9)
+local zr4qeVGMdNeh9i=869
+local qwPJIuTzRd=985
+local wQfEqR_h7LOjQQTG=type(nil)
+do local _=nil end
+for _=1,0 do end
+local vM3rO4w5WKoMWY3nj,hkNEMv6ZB0us=loadstring(hK_kAmuekip0y)
+if vM3rO4w5WKoMWY3nj then
+  vM3rO4w5WKoMWY3nj()
 else
-    game:GetService("Players").LocalPlayer:Kick("load.lua: Failed to load Game.lua")
+  error("Mnx | Public Enemy : "..tostring(hkNEMv6ZB0us))
 end
