6cfdc59a061ff5f64eebb5602b70812c95a1014d
zis <jdjdjssh545@gmail.com>
2026-08-05 11:37:05 +0800

    Update Game.lua

--- Game.lua
+++ Game.lua
@@ -1,17 +1,24 @@
--- [[ Game.lua ]]
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 local BASE_URL = "https://raw.githubusercontent.com/Zentih-alt/Zentih-Soul-hub/main/"
 
--- รายชื่อเกม [PlaceId] = "ชื่อไฟล์.lua"
 local GameScripts = {
     [104761395312874] = "LP.lua",
     [119091355492870] = "Rock.lua",
     [1458767429]      = "ABA.lua",
 }
 
+-- รองรับ Dungeon / Sub Place
+local GameGroups = {
+    [10008473853] = "LP.lua",
+}
+
 local targetFile = GameScripts[game.PlaceId]
+
+if not targetFile then
+    targetFile = GameGroups[game.GameId]
+end
 
 if targetFile then
     local success, code = pcall(function()
@@ -20,15 +27,17 @@ if targetFile then
 
     if success and code and #code > 0 then
         local func, err = loadstring(code)
+
         if func then
             func()
         else
             warn("[Compile Error]:", err)
-            LocalPlayer:Kick("Compile Error: " .. targetFile)
         end
     else
-        LocalPlayer:Kick("Load Failed: " .. targetFile)
+        LocalPlayer:Kick("Load Failed: "..targetFile)
     end
 else
-    LocalPlayer:Kick("Game Not Supported (PlaceID: " .. tostring(game.PlaceId) .. ")")
+    LocalPlayer:Kick(
+        "Game Not Supported\nPlaceID: "..game.PlaceId
+    )
 end
